4b50575a2c4c10f4acc3b8a3e59f7d15ab559eb2
Kaio Oliveira <kaiohenrique2601@gmail.com>
2021-04-15 18:09:51 -0300

    feat mega-sena points

--- mega_sena/urls.py
+++ mega_sena/urls.py
@@ -1,7 +1,8 @@
-from .views import GameView,ResultView
+from .views import GameView,ResultView, PointsView
 from django.urls import path
 
 urlpatterns = [
     path('game/', GameView.as_view()),
     path('result/', ResultView.as_view()),
+    path('points/', PointsView.as_view()),
 ]
--- mega_sena/views.py
+++ mega_sena/views.py
@@ -1,14 +1,13 @@
 import cfscrape
 import random
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from bs4 import BeautifulSoup
-from lxml import etree
 
 from usuario.services import validate_token_user
 from .models import Game
 from .serializers import GameSerializer
+from datetime import datetime, timedelta
 
 
 class GameView(APIView):
@@ -18,24 +17,28 @@ class GameView(APIView):
         if valid['response'] != True:
             return Response({'response': valid['response']}, valid['status'])
         user_object = valid['user']
-        
-        games = GameSerializer(Game.objects.filter(user=user_object),many=True).data
-        
+
+        games = GameSerializer(Game.objects.filter(
+            user=user_object), many=True).data
+
         return Response(games)
 
     def post(self, request):
-        valid = validate_token_user(request.headers)
+        if request.data['tens'] < 6 or request.data['tens'] > 10:
+            return Response('numero de dezenas invalido', 400)
 
+        valid = validate_token_user(request.headers)
         if valid['response'] != True:
             return Response({'response': valid['response']}, valid['status'])
-        
+
         numbers = str(sorted([random.randint(1, 60)
-                         for i in range(request.data['tens'])]))
+                              for i in range(request.data['tens'])]))
         user_object = valid['user']
-        game_object = Game.objects.create(numbers=numbers,user=user_object)
+        game_object = Game.objects.create(numbers=numbers, user=user_object)
         game = GameSerializer(game_object).data
 
         return Response(game)
+
 
 class ResultView(APIView):
     def get(self, request):
@@ -47,6 +50,7 @@ class ResultView(APIView):
         url = "https://www.google.com/search?q=resultado+mega+sena"
         scraper = cfscrape.create_scraper()
         data = scraper.get(url).content
+        data = data.decode('utf-8')
         soup = BeautifulSoup(data, "html.parser")
 
         concurse = soup.find("span", class_="qLLird").span.text
@@ -54,3 +58,42 @@ class ResultView(APIView):
             soup.find("div", class_="MDTDab").children)]
 
         return Response({"concurso": concurse, "numeros": concurse_result})
+
+
+class PointsView(APIView):
+    def get(self, request):
+        valid = validate_token_user(request.headers)
+
+        if valid['response'] != True:
+            return Response({'response': valid['response']}, valid['status'])
+
+        user_object = valid['user']
+
+        url = "https://www.google.com/search?q=resultado+mega+sena"
+        scraper = cfscrape.create_scraper()
+        data = scraper.get(url).text
+        soup = BeautifulSoup(data, "html.parser")
+        concurse = soup.find("span", class_="qLLird").span.text
+        concurse_date = datetime.strptime(
+            concurse.split()[2].strip('(').strip(')'), '%d/%m/%y')
+
+        game = GameSerializer(Game.objects.filter(
+            user=user_object), many=True).data[-1]
+        game_date = datetime.strptime(game['date'],'%Y-%m-%d')
+        
+        if game_date < concurse_date:
+            return Response('resultado ainda está indisponivel')
+
+        concurse_result = [int(span.text) for span in list(
+            soup.find("div", class_="MDTDab").children)]
+
+        game_numbers = [int(number) for number in game['numbers'].strip(
+            '[').strip(']').split(',')]
+
+        points = 0
+
+        for number in game_numbers:
+            if number in concurse_result:
+                points += 1
+
+        return Response({'points': points, "game": game,"concurse": concurse, "result_numbers": concurse_result, "game_numbers": game_numbers})
