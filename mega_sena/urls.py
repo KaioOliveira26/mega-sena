1c27050abe00ecc3503fab94ffd46bdc972b673d
Kaio Oliveira <kaiohenrique2601@gmail.com>
2021-04-15 18:43:08 -0300

    feat:routes in readme

--- mega_sena/urls.py
+++ mega_sena/urls.py
@@ -2,7 +2,7 @@ from .views import GameView,ResultView, PointsView
 from django.urls import path
 
 urlpatterns = [
-    path('game/', GameView.as_view()),
+    path('', GameView.as_view()),
     path('result/', ResultView.as_view()),
     path('points/', PointsView.as_view()),
 ]
